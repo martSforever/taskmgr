7885071cc34b7959df2f563018110425703192e5
martSforever <marts_forever@163.com>
2017-09-26 16:06:00 +0800

    忘了提交新添加的确认对话框

---
+++ src/app/shared/confirm-dialog/confirm-dialog.component.ts
@@ -0,0 +1,38 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import {MD_DIALOG_DATA, MdDialogRef} from "@angular/material";
+
+@Component({
+  selector: 'app-confirm-dialog',
+  template: `
+    <form>
+      <h2 md-dialog-title>{{title}}</h2>
+      <div md-dialog-content>
+        {{content}}
+      </div>
+      <div md-dialog-actions>
+        <button type="button" md-raised-button color="primary" (click)="onSave(true)">保存</button>
+        <button type="button" md-button md-dialog-close (click)="onSave(false)">取消</button>
+      </div>
+    </form>
+
+  `,
+  styles: []
+})
+export class ConfirmDialogComponent implements OnInit {
+
+  title = '';
+  content = '';
+
+  constructor(private dialogRef: MdDialogRef<ConfirmDialogComponent>, @Inject(MD_DIALOG_DATA) private data) {
+  }
+
+  ngOnInit() {
+    this.title = this.data.title;
+    this.content = this.data.content;
+  }
+
+  onSave(flag: boolean) {
+    this.dialogRef.close(flag);
+  }
+
+}
